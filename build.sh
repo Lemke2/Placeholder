941c4dee153ff0e3467f244bd362fee656a006bb
Lemke2 <lazarlemic2@gmail.com>
2025-11-15 22:28:30 +0100

    asd

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@
 CC=clang
 RAYLIB_INCLUDE="/usr/local/include/raylib"  # Path to raylib.h
 RAYLIB_LIB="/usr/local/include/raylib/src"  # Path to libraylib.a
-OUTPUT="ArenaVisualizer"
+OUTPUT="game.exe"
 
 # Compile command for static linking
-$CC src/*.c -I$RAYLIB_INCLUDE -L$RAYLIB_LIB -l:libraylib.a \
+$CC src/main.c -I$RAYLIB_INCLUDE -L$RAYLIB_LIB -l:libraylib.a \
     -lGL -lm -lpthread -ldl -lX11 -o $OUTPUT
 
 if [ $? -eq 0 ]; then
--- src/main.c
+++ src/main.c
@@ -2,6 +2,27 @@
 #include <stdlib.h>
 #include "raylib.h"
 
-int main(){
-    printf("Hello world");
+#define global static
+
+int main(void)
+{
+    global const int screenWidth = 960;
+    global const int screenHeight = 540;
+
+    InitWindow(screenWidth, screenHeight, "Placeholder - waiting for game name");
+
+    while (!WindowShouldClose())
+    {
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+
+    
+
+
+
+            
+    EndDrawing();
+    }
+    CloseWindow();
+    return 0;
 }
